00792e4bde691f7d6362276992943f7a7174ef2f
Young jun Kim <youngjunkim@Youngui-MacBookPro.local>
2020-07-20 03:38:11 +0900

    Node Module Upload

---
+++ src/main/frontend/build/precache-manifest.0becfa789ae269c541a74f2f3b3f2099.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e2fecbf0fc6fdccc2f8b6936e46950",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbbd482a540eaf2114a7",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c91a80b3d219d67fde8a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dbbd482a540eaf2114a7",
+    "url": "/static/js/2.6a6a75cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6a6a75cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c91a80b3d219d67fde8a",
+    "url": "/static/js/main.7f023976.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import TopMenuComponent from "./jsx/TopMenuComponent";
+
+function App() {
+  return (
+    <div class="App">
+      <div>
+        <TopMenuComponent>
+
+        </TopMenuComponent>
+      </div>
+    </div>
+  );
+}
+
+export default App;
